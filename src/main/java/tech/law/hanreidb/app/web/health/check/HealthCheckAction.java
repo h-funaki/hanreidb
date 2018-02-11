6606fae75160a94f8e4eea7a8c429af644de4de5
h-funaki <h-funaki@funakihatsunoMacBook-Pro.local>
2018-02-11 13:05:31 +0900

    no message

--- src/main/java/tech/law/hanreidb/app/web/health/check/HealthCheckAction.java
+++ src/main/java/tech/law/hanreidb/app/web/health/check/HealthCheckAction.java
@@ -1,5 +1,8 @@
 package tech.law.hanreidb.app.web.health.check;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import javax.annotation.Resource;
 
 import org.lastaflute.web.Execute;
@@ -31,6 +34,15 @@ public class HealthCheckAction extends HanreidbBaseAction {
         result.env_title = env.getEnvironmentTitle();
         result.db_ip = env.getJdbcUrl();
         result.cors_url = env.getCorsUrl();
+
+        try {
+            InetAddress addr = InetAddress.getLocalHost();
+            result.local_host_name = addr.getHostName();
+            result.ip_address = addr.getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
         return asJson(result);
     }
 
@@ -43,5 +55,9 @@ public class HealthCheckAction extends HanreidbBaseAction {
         public String db_ip;
 
         public String cors_url;
+
+        public String local_host_name;
+
+        public String ip_address;
     }
 }
