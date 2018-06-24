d10fdf600f5283abe9b477ff41b5ce95efb2df4e
h-funaki <h-funaki@funakihatsunoMacBook-Pro.local>
2018-06-24 12:28:30 +0900

    法令内容を見やすく

--- src/main/java/tech/law/hanreidb/app/web/law/get/LawGetAction.java
+++ src/main/java/tech/law/hanreidb/app/web/law/get/LawGetAction.java
@@ -2,6 +2,7 @@ package tech.law.hanreidb.app.web.law.get;
 
 import static tech.law.hanreidb.app.base.util.UnextStaticImportUtil.newArrayList;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -13,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import tech.law.hanreidb.app.base.HanreidbBaseAction;
+import tech.law.hanreidb.app.logic.FileLogic;
 import tech.law.hanreidb.app.web.law.get.LawGetContentResult.LawCategoryPart;
 import tech.law.hanreidb.app.web.law.get.LawGetContentResult.SourcePart;
 import tech.law.hanreidb.dbflute.exbhv.LawBhv;
@@ -44,12 +46,14 @@ public class LawGetAction extends HanreidbBaseAction {
     private LawBhv lawBhv;
     @Resource
     private RemoteEgovBhv remoteEgovBhv;
+    @Resource
+    private FileLogic fileLogic;
 
     // ===================================================================================
     //                                                                             Execute
     //                                                                             =======
     @Execute
-    public JsonResponse<LawGetContentResult> index(String law_public_code) {
+    public JsonResponse<LawGetContentResult> index(String law_public_code) throws IOException {
         return asJson(mappingToContent(selectLaw(law_public_code)));
     }
 
@@ -79,7 +83,7 @@ public class LawGetAction extends HanreidbBaseAction {
     // ===================================================================================
     //                                                                             Mapping
     //                                                                             =======
-    private LawGetContentResult mappingToContent(Law law) {
+    private LawGetContentResult mappingToContent(Law law) throws IOException {
         LawGetContentResult content = new LawGetContentResult();
         content.law_public_code = law.getLawPublicCode();
         content.law_name = law.getLawName();
@@ -116,36 +120,79 @@ public class LawGetAction extends HanreidbBaseAction {
         content.law_amend_version = lawHistory.getLawAmendVersion();
         content.latest_version_flg = lawHistory.getLatestVersionFlg();
 
+        //        content.law_content_view_xml =
+        //                toXml(fileLogic.readAll("/Users/h-funaki/Documents/hanreidb/src/test/resources/job/law/content/raw_xml_2.xml"));
         LawContent lawContent = lawHistory.getLawContentAsOne().get();
-
         content.law_content_view_xml = toXml(lawContent.getLawContentViewXml());
 
         return content;
     }
 
     public String toXml(String rawXml) {
-        String viewXml = rawXml.replaceFirst("<LawTitle>.*</LawTitle>", "") // 法令名 e.g. 牧野法
+        String viewXml = rawXml.substring(rawXml.indexOf("<LawBody>"), rawXml.indexOf("</LawBody>") + 10) // LawBodyを取り出す。 
+                // -----------------------------------------------------
+                //                                                   目次
+                //                                                   ---
+                // タイトル
+                .replaceFirst("<LawTitle>.*</LawTitle>", "") // 法令名 e.g. 牧野法
+                // 制定文
+                .replaceAll("<EnactStatement>", "<br><EnactStatement>") //
+                // 目次
                 .replaceFirst("<TOCLabel>目次</TOCLabel>", "") // "目次"
-                .replaceAll("(<TOCChapter[\"= a-zA-Z0-9]* Num=\")(\\d+)(\">)", "<a href=\"#chapter$2\">$1$2$3") // 目次のリンク(開始タグ)
-                .replaceAll("</TOCChapter>", "</TOCChapter></a>") // 目次のリンク(終了タグ)
-                .replaceAll("</ArticleRange>", "</ArticleRange><br>") // 目次の条文範囲ごとの改行
-                .replaceFirst("<TOCSupplProvision>", "<h5><TOCSupplProvision><a>") // 附則のリンク(開始タグ)
-                .replaceFirst("</TOCSupplProvision>", "</a></TOCSupplProvision></h5><br>") // 附則のリンク(終了タグ)
-                .replaceAll("<ArticleRange>", "<h5><ArticleRange>")
-                .replaceAll("</ArticleRange>", "</ArticleRange></h5>")
-                // ここからメイン
-                .replaceFirst("<MainProvision>>", "<MainProvision><br><br>") // 目次とメインの間
-                .replaceFirst("</TOC>", "</TOC><br>") //
-                .replaceAll("</ArticleCaption>", "</ArticleCaption><br>") //
-                .replaceAll("</Sentence>", "</Sentence><br>")
-                .replaceAll("</Chapter>", "</Chapter><br><br>") // 章の終わり
-                .replaceAll("<ChapterTitle>", "<h5><ChapterTitle>") // 
-                .replaceAll("</ChapterTitle>", "</ChapterTitle></h5>") // 
-                .replaceFirst("</MainProvision>>", "</MainProvision><br><br>") // メインと附則の間
-                .replaceAll("(<Chapter[\"= a-zA-Z0-9]* Num=\")(\\d+)(\">)", "$1$2\" id=\"chapter$2$3") // e.g. " id=chapter2"
-                // ここから附則
-                .replaceAll("<SupplProvisionLabel>附　則</SupplProvisionLabel>", "<br><SupplProvisionLabel><h5>附　則</h5></SupplProvisionLabel>") // "附則"
-        ;
+                .replaceAll("", "") //
+                // 編
+                .replaceAll("(<TOCPart[\"= a-zA-Z0-9]* Num=\")(\\d+)(\">)", "<br><h4>$1$2$3<a href=\"#part$2\">") // 目次の編(開始タグ)
+                .replaceAll("</PartTitle>", "</PartTitle></a></h4>") // 目次の編(終了タグ)
+                // 章
+                .replaceAll("(<TOCChapter[\"= a-zA-Z0-9]* Num=\")(\\d+)(\">)", "<br><h5><a href=\"#chapter$2\">$1$2$3") // 目次の章(開始タグ)
+                .replaceAll("</TOCChapter>", "</TOCChapter></a></h5>") // 目次の章(終了タグ)
+                // 節
+                .replaceAll("(<TOCSection[\"= a-zA-Z0-9]* Num=\")(\\d+)(\">)", "<br><h5><a href=\"#section$2\">$1$2$3") // 目次の節(開始タグ)
+                .replaceAll("</TOCSection>", "</TOCSection></a></h5>") // 目次の節(終了タグ)
+                // 款
+                .replaceAll("(<TOCSubsection[\"= a-zA-Z0-9]* Num=\")(\\d+)(\">)", "<br><h6><a href=\"#subsection$2\">$1$2$3") // 目次の款(開始タグ)
+                .replaceAll("</TOCSubsection>", "</TOCSubsection></a></h6>") // 目次の款(終了タグ)
+                // 目
+                .replaceAll("(<TOCDivision[\"= a-zA-Z0-9]* Num=\")(\\d+)(\">)", "<br><h6><a href=\"#division$2\">$1$2$3") // 目次の目(開始タグ)
+                .replaceAll("</TOCDivision>", "</TOCDivision></a></h6>") // 目次の目(終了タグ)
+                // 
+                .replaceFirst("<TOCSupplProvision>", "<br><h5><TOCSupplProvision><a>") // 附則のリンク(開始タグ)
+                .replaceFirst("</TOCSupplProvision>", "</a></TOCSupplProvision></h5>") // 附則のリンク(終了タグ)
+                // -----------------------------------------------------
+                //                                                 メイン
+                //                                                 -----
+                .replaceFirst("", "") //
+                .replaceAll("", "") //
+                .replaceAll("<ArticleCaption>", "<br><ArticleCaption>") //
+                .replaceAll("<ArticleTitle>", "<br><ArticleTitle>") //
+                //.replaceAll("<Article([ >])", "<br><Article$1") //
+                .replaceAll("<ParagraphNum(/?)", "<br><ParagraphNum$1")
+                .replaceAll("(<Part[\"= a-zA-Z0-9]* Num=\")(\\d+)(\">)", "<br>$1$2\" id=\"part$2$3") // e.g. " id=part2"
+                .replaceAll("(<Chapter[\"= a-zA-Z0-9]* Num=\")(\\d+)(\">)", "<br>$1$2\" id=\"chapter$2$3") // e.g. " id=chapter2"
+                .replaceAll("(<Section[\"= a-zA-Z0-9]* Num=\")(\\d+)(\">)", "<br>$1$2\" id=\"section$2$3") // e.g. " id=section2"
+                .replaceAll("(<Subsection[\"= a-zA-Z0-9]* Num=\")(\\d+)(\">)", "<br>$1$2\" id=\"subsection$2$3") // e.g. " id=subsection2"
+                .replaceAll("(<Division[\"= a-zA-Z0-9]* Num=\")(\\d+)(\">)", "<br>$1$2\" id=\"division$2$3") // e.g. " id=division2"
+                // メインと附則の間
+                .replaceFirst("</MainProvision>>", "</MainProvision><br><br>")
+                // -----------------------------------------------------
+                //                                                   附則
+                //                                                   ---       
+                .replaceAll("<SupplProvisionLabel>附　則</SupplProvisionLabel>",
+                        "<br><SupplProvisionLabel><h5>附　則</h5></SupplProvisionLabel><br>") // "附則"
+                .replaceAll("</SupplProvision>", "</SupplProvision>")
+                // -----------------------------------------------------
+                //                                               テーブル
+                //                                               -------
+                .replaceAll("<Table>", "<br><table class=\"ui fixed single line celled table\">")
+                .replaceAll("<TableRow>", "<tr>")
+                .replaceAll("</TableRow>", "</tr>")
+                .replaceAll("<TableColumn>", "<td>")
+                .replaceAll("</TableColumn>", "</td>")
+                // -----------------------------------------------------
+                //                                                   全体
+                //                                                   ---
+                .replaceAll("<br>\n( *)([</a-zA-Z0-9>]*)<br>", "<br>\n$1$2");
+
         logger.debug(viewXml);
         return viewXml;
     }
